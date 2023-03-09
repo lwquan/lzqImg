ca90095e3388265faa9c48fe3bbbe01c50ad182d
兰文权 <12058591+lzq1234511@user.noreply.gitee.com>
2023-03-09 15:22:48 +0800

    1

--- waterfall.js
+++ waterfall.js
@@ -35,16 +35,16 @@ WaterFall.prototype = {
 		styleE.innerHTML = `div.item{
         position: absolute;
         transition: all .5s;  /* 使过渡平滑*/
-        width : 200px;
+        width : 187px;
         height: auto;
-        padding:5px;
+		padding:5px;
         box-sizing: border-box;  /* 非常重要，可将不必要的计算略去 */
 		opacity:0;
       }
       div.item img{
         width: 100%;
-        height: auto;
-	
+		height: auto;
+
       }
       .forcenter{
         position: relative;
@@ -55,23 +55,22 @@ WaterFall.prototype = {
 	},
 	resizeWF: function() {
 		let winW = window.innerWidth;
-		let itemNum = Math.floor(winW / 200); // 当每一个项的宽度都是固定的时候，需要计算出浏览器一行可以排列几个。
-		this.container.style.width = (itemNum * 200) + 'px'; // 用于居中的包裹盒子的宽度
+		let itemNum = Math.floor(winW / 187); // 当每一个项的宽度都是固定的时候，需要计算出浏览器一行可以排列几个。
+		this.container.style.width = (itemNum * 187) + 'px'; // 用于居中的包裹盒子的宽度
 
 		let saveColumnHeight = []; // 定义一个数组，用于存储 每一列所有元素的高度 之和
 		let items = document.querySelectorAll('.item');
 
 		for (let i = 0; i < items.length; i++) {
-			
+
 			if (saveColumnHeight.length < itemNum) {
 				saveColumnHeight[i] = items[i].offsetHeight; // 当布局的元素还没占满一行时，继续向数组中添加第一行第i列的高度
-				setDiv(items[i], 200 * i, 0); // 放置div
+				setDiv(items[i], 187 * i, 0); // 放置div
 				
 			} else { // 当已经占满一行时，就找出每一列的最小高度，然后当前的这个div放在高度最小的那一列
 				let pos = getMinH(saveColumnHeight); //去找高度最小的那一列
 				saveColumnHeight[pos.column] += items[i].offsetHeight;
 				setDiv(items[i], pos.left, pos.top); // 放置div
-				
 			}
 			
 			$(items[i]).addClass(i%2?'fadeInRight':'fadeInLeft').attr('animationName',i%2?'fadeInRight':'fadeInLeft');
@@ -111,12 +110,11 @@ function getMinH(arr) {
 		} else {
 			if (flag.top > item) { // 找出高度最小的那一列
 				flag.top = item;
-				flag.left = 200 * i;
+				flag.left = 187 * i;
 				flag.column = i;
 			}
 		}
 	});
-
 	return flag;
 }
 
@@ -124,5 +122,5 @@ function setDiv(item, left, top) {
 	item.style.left = left + 'px';
 	item.style.top = top + 'px';
 	item.style.animationName = 'none';
-	item.style.opacity = '1';
+	item.style.opacity = '.05';
 }
